4b921b9e5fb54492766b984a5568c0739792ad85
Cas6erJack <casperjack616@gmail.com>
2026-05-21 15:52:45 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,21 @@
 
+import "./../styles/globals.css";
+import Navbar from "./components/Navbar";
+
+export const metadata = {
+  title: "Greenland",
+  description: "Calm savings + plant growth tracker"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <div style={{ maxWidth: 500, margin: "0 auto", padding: 16 }}>
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
