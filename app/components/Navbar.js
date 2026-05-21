df2597d0ce6859ebd8785095377de48d0f58f4ee
Cas6erJack <casperjack616@gmail.com>
2026-05-21 16:12:10 -0400

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,1 +1,16 @@
 
+"use client";
+
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div style={{ display: "flex", gap: 10, padding: 10 }}>
+      <Link href="/">Home</Link>
+      <Link href="/tasks">Tasks</Link>
+      <Link href="/shop">Shop</Link>
+      <Link href="/plants">Plants</Link>
+      <Link href="/stats">Stats</Link>
+    </div>
+  );
+}
